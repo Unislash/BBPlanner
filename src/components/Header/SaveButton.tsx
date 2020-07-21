ccedf027f3304c2c89a1ce27db590b52d51c7142
Brent <brent.m.grossman@gmail.com>
2020-07-21 01:12:37 -0700

    Save button will change icons when a build is saved

--- src/components/Header/SaveButton.tsx
+++ src/components/Header/SaveButton.tsx
@@ -1,31 +1,47 @@
 import * as React from 'react';
-import SaveIcon from '@material-ui/icons/SaveAlt';
+import SaveIcon from '@material-ui/icons/Favorite';
+import SaveIconBorder from '@material-ui/icons/FavoriteBorder';
 import {connect} from 'react-redux';
 import {saveBuild} from '../../thunks';
+import {AppState} from '../../models';
+import classcat from 'classcat';
 
 interface SaveButtonProps {
+    buildName: string;
+    buildIdList: string[];
     saveBuild: () => void;
 }
 
-export const BaseSaveButton: React.FC<SaveButtonProps> = ({saveBuild}) => {
-
+export const BaseSaveButton: React.FC<SaveButtonProps> = ({
+    saveBuild,
+    buildName,
+    buildIdList,
+}) => {
+    const isSaved = buildIdList.indexOf(buildName) > -1;
     return (
         <div
-            className="saveButton"
+            className={classcat(['saveButton', {
+                saved: isSaved
+            }])}
             onClick={() => {
                 saveBuild()
             }}
         >
-            <SaveIcon/>
+            {isSaved ? <SaveIcon/> : <SaveIconBorder/>}
         </div>
     );
 };
+
+const mapStateToProps = (state: AppState) => ({
+    buildName: state.buildName,
+    buildIdList: state.buildIdList,
+});
 
 const mapDispatchToProps = {
     saveBuild,
 };
 
 export const SaveButton = connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps,
 )(BaseSaveButton);
