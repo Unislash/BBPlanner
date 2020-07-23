5eb6a7aab193233ae976d370120e8cd61a467565
Brent <brent.m.grossman@gmail.com>
2020-07-23 00:15:04 -0700

    Add another link to Other Resources

--- src/components/OtherResources/OtherResources.tsx
+++ src/components/OtherResources/OtherResources.tsx
@@ -11,6 +11,7 @@ export const OtherResources: React.FC = () => {
                 <p><a href="https://battlebrothers.fandom.com/wiki/Battle_Brothers_Wiki">Battle Brothers Wiki<HyperlinkIcon /></a></p>
                 <p><a href="https://bbbros.herokuapp.com">"Effective HP" Calculator<HyperlinkIcon /></a></p>
                 <p><a href="https://github.com/turtle225/Battle-Brothers-Damage-Calculator">Multi-Enemy Combat Simulator<HyperlinkIcon /></a></p>
+                <p><a href="https://wlirareddit.github.io/bb_calculator/seed_search.html">Seed Search by Wlira<HyperlinkIcon /></a></p>
                 <p><a href="https://www.nexusmods.com/battlebrothers/mods">Mods on Nexus<HyperlinkIcon /></a></p>
                 <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Battle Brothers Reddit<HyperlinkIcon /></a></p>
                 <p><a href="http://battlebrothersgame.com">Battle Brothers Website<HyperlinkIcon /></a></p>
